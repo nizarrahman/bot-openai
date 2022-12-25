3e8e8c44346365046432567b269173427c4f3108
Nizar Rahman <102470420+nizarrahman@users.noreply.github.com>
2022-12-25 18:09:06 +0700

    Update index.js

--- index.js
+++ index.js
@@ -24,8 +24,13 @@ client.on('message', async msg => {
     const text = msg.body.toLowerCase() || '';
 
     //check status
-    if (text === '!ping') {
-        msg.reply('pong');
+    if (text === '!menu , .menu , #menu , menu') {
+        msg.reply('Ini Fitur Dari Kami');
+        msg.reply('#edit_bg');
+        msg.reply('#ask/');
+        msg.reply('Untuk Edit_bg Itu Buat Edit Background Gambar Contoh kirim foto dengan caption edit_bg/warna
+Dan Untuk #ask/ Untuk Bertanya Kepada Open Ai contoh #ask/pertanyaannya Paham Kan.
+Jika Menemukan Bug Harap Lapor Ke Saya https://wa.me/6281386945391'); 
     }
 
     // edit_bg/bg_color
